7e2870131585b6aba38e7adc2e38237a0c7ee2bb
saklain404 <mohdsaklain8899@gmail.com>
2022-05-08 11:30:04 +0530

    blank screen fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,36 @@
 import { Button } from 'antd';
-import React from 'react'
-import {reactLocalStorage} from 'reactjs-localstorage';
+import React, {useEffect, useState} from 'react'
+import { reactLocalStorage } from 'reactjs-localstorage';
+import {initAxios} from "../helpers/login"
 
 import Logo from "./Logo.png"
 
 
 function Header({ userData }) {
+  const [isAccess, setisAccess] = useState('')
   const logout = () => {
     reactLocalStorage.clear();
     window.location.reload(false)
   }
+  useEffect(() => {
+    init();
+  }, [])
+
+  const init = async () => {
+    const auth = await initAxios();
+    const { access, user } = auth;
+    setisAccess(access);
+  }
+
+ 
   return (
     <div className='header bg-white'>
       <img src={Logo} alt='logo' />
       <div className='header-right'>
         <h4 className='username'>{userData?.username}</h4>
-        <Button onClick={logout}>Log out</Button>
+
+        {isAccess ?  <Button onClick={logout}>Log out</Button> :  <p></p> }
+
       </div>
 
     </div>
